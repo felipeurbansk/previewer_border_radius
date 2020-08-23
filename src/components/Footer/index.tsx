1f570de3711a6c5f7fc55d7ec40fc4162da580c4
Felipe Urbanski <felipe@dealersites.com.br>
2020-08-22 23:49:53 -0300

    Ended application border-radius controll

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return(
         <footer>
             <section className="content-footer">
-                <span>Project inspirated on <span className="primary">florinpop17/app-ideas</span>.</span>
+                <span>Application inspired by <span className="primary"><a href="https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Border-Radius-Previewer.md">florinpop17/app-ideas</a></span>.</span>
             </section>
         </footer>
     )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,14 +16,15 @@ const Home = () => {
         <div className="home-content">
             <Header/>
             <main>
+                <code className="code">border-radius: {top}% {right}% {bottom}% {left}%</code>
                 <div className="controllers-radius">
-                    <input className="slider-top" type="range" min="1" max="100" value={top} onChange={ (e) => { setTop(e.target.value); } } />
-                    <input className="slider-right" type="range" min="1" max="100" value={right} onChange={ (e) => { setRight(e.target.value); } } />
+                    <input className="input-slider slider-top" type="range" min="1" max="100" value={top} onChange={ (e) => { setTop(e.target.value); } } />
+                    <input className="input-slider slider-right slider-vertical" type="range" min="1" max="100" value={right} onChange={ (e) => { setRight(e.target.value); } } />
 
                     <Box top={top} right={right} bottom={bottom} left={left} />
 
-                    <input className="slider-bottom" type="range" min="1" max="100" value={bottom} onChange={ (e) => { setBottom(e.target.value); } } />
-                    <input className="slider-left" type="range" min="1" max="100" value={left} onChange={ (e) => { setLeft(e.target.value); } } />
+                    <input className="input-slider slider-bottom" type="range" min="1" max="100" value={bottom} onChange={ (e) => { setBottom(e.target.value); } } />
+                    <input className="input-slider slider-left slider-vertical" type="range" min="1" max="100" value={left} onChange={ (e) => { setLeft(e.target.value); } } />
                 </div>
             </main>
             <Footer/>
